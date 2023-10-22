149553ec5978556425551b3a073178041f8c4563
George D <georgedutra661@gmail.com>
2023-10-22 18:36:45 -0300

    organize files

--- src/q_learning.py
+++ src/q_learning.py
@@ -33,6 +33,9 @@ class QL_Tetris:
     def reset_q_table(self, shape):
         self.q_table = np.random.uniform(low = -2, high = 0, size = shape) # Populating the initial q-table with random values
 
+    def load_q_table(self, filename):
+        self.q_table = np.load(f"./qtables/{filename}")
+
     def plot_rewards(self):
         plt.plot(self.aggr_ep_rewards['ep'], self.aggr_ep_rewards['avg'], label="average rewards")
         plt.plot(self.aggr_ep_rewards['ep'], self.aggr_ep_rewards['max'], label="max rewards")
@@ -45,31 +48,24 @@ class QL_Tetris:
         self.total_rewards = []
         self.aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
 
-    def train(self, learn_rate, discount, max_episodes, epsilon, eps_interval = "half", target = np.inf, save_q_table = "final"):
+    def train(self, learn_rate, discount, max_episodes, target = np.inf, save_q_table = "final", plot = True):
         self.clean_rewards()
-        
+
         # epsilon is the exploration parameter; the bigger his value, the bigger the chance for the environment to make an exploratory action
+        epsilon = 1
         start_epsilon_decay = 1
-
-        if eps_interval == "half":
-            end_epsilon_decay = max_episodes/2
-            epsilon_decay_value = epsilon/(end_epsilon_decay - start_epsilon_decay)
+        end_epsilon_decay = max_episodes
+        epsilon_decay_value = epsilon/(end_epsilon_decay - start_epsilon_decay)
         
-        elif eps_interval == "full":
-            end_epsilon_decay = max_episodes
-            epsilon_decay_value = epsilon/(end_epsilon_decay - start_epsilon_decay)
-
         # Main training loop
         for episode in range(max_episodes+1): # Outer loop, referes to training epochs
             episode_reward = 0
-
-            if not episode%100: print(episode)
 
             # Intermediate episodes where the environment will show current results
             if episode % self.render_interval == 0: show_render = True
             else: show_render = False
 
-            # First we take the possible next states
+            # First we take the possible states
             next_steps = self.env.get_next_states()
             next_actions, next_states = zip(*next_steps.items())
             next_states = torch.stack(next_states)
@@ -82,26 +78,26 @@ class QL_Tetris:
             
             done = False
             while not done: # Inner loop, refering to a single game
-
                 if np.random.random() > epsilon: index = np.argmax(q_values) # Policy Step
                 else: index = np.random.randint(0, len(q_values)) # Exploratory Step
                 
                 action = next_actions[index] 
 
                 # Applies the best action
-                score, done = self.env.step(action, render = show_render, video = self.out)
-                reward = score - 2
-
+                reward, done = self.env.step(action, render = show_render, video = self.out)
                 episode_reward += reward
 
-                if self.env.score > target: # If we reach this score, we're satisfied with this episode
+                # save the current state's location
+                current_q_loc = next_states[index].int()
+
+                if done: # the model lost, and the q_value must be discouraged
+                    self.q_table[current_q_loc[0]][current_q_loc[1]][current_q_loc[2]][current_q_loc[3]] = -2 
+
+                elif self.env.score > target: # If we reach this score, we're satisfied with this episode
                     done = True
                     print(f"Achieved victory in episode {episode}!!!")
 
-                if not done: # If the episode isn't finished, update the q_values
-                    # Start by saving the current state's location
-                    current_q_loc = next_states[index].int()
-
+                else: # If the episode isn't finished yet, update the q_values
                     # Search for the best action in the next step
                     next_steps = self.env.get_next_states()
                     next_actions, next_states = zip(*next_steps.items())
@@ -116,7 +112,6 @@ class QL_Tetris:
 
                     # Finally calculate the new value for the current state
                     new_q = (1 - learn_rate) * self.q_table[current_q_loc[0]][current_q_loc[1]][current_q_loc[2]][current_q_loc[3]] + learn_rate * (reward + discount * max_future_q)
-                    # TODO: Problem, we don't have a well-defined reward :/
 
                     self.q_table[current_q_loc[0]][current_q_loc[1]][current_q_loc[2]][current_q_loc[3]] = new_q # Insert the updated value into the table
             
@@ -125,13 +120,15 @@ class QL_Tetris:
                 epsilon -= epsilon_decay_value
 
             self.total_rewards.append(episode_reward)
+            print(f"Episode {episode}/{max_episodes} reward: {episode_reward}")
+            
             if not episode % self.stats_interval:
                 average_reward = sum(self.total_rewards[-self.stats_interval:])/self.stats_interval
                 self.aggr_ep_rewards['ep'].append(episode)
                 self.aggr_ep_rewards['avg'].append(average_reward)
                 self.aggr_ep_rewards['max'].append(max(self.total_rewards[-self.stats_interval:]))
                 self.aggr_ep_rewards['min'].append(min(self.total_rewards[-self.stats_interval:]))
-                print(f'Episode: {episode:>5d}, average reward: {average_reward:>4.1f}, current epsilon: {epsilon:>1.2f}')
+                print(f"Relatory: Max Reward: {self.aggr_ep_rewards['max'][-1]}, Average Reward: {average_reward}, Min Reward: {self.aggr_ep_rewards['min'][-1]}, Current Epsilon: {epsilon}")
                 
             if save_q_table == "stats" and not episode % self.stats_interval:
                 np.save(f"qtables/{episode}-qtable.npy", self.q_table)
@@ -141,13 +138,14 @@ class QL_Tetris:
 
             self.env.reset() # Resets environment after each episode
 
-        self.plot_rewards()
         cv2.destroyAllWindows()
+        if plot: self.plot_rewards()
 
+    # def parameter_analysis(self):
+    #     for discount in range(0.1, 1, 0.1)
 
-q_model = QL_Tetris()
-q_model.train(learn_rate=0.3,
-              discount=0.9,
-              max_episodes=10000,
-              epsilon=0.5,
-              eps_interval="full")
+if __name__ == "__main__":
+    q_model = QL_Tetris()
+    q_model.train(learn_rate=0.3,
+                discount=0.9,
+                max_episodes=20000)
