34c383ed04da4fce364d2b74771c5f7c35f694f5
Zuilho Segundo <segundozuilho@gmail.com>
2023-10-22 17:19:39 -0300

    finish

---
+++ src/teste.py
@@ -0,0 +1,50 @@
+import torch
+import cv2
+from tetris import Tetris
+
+WIDTH = 10
+HEIGHT = 20
+BLOCK_SIZE = 30
+FPS = 300
+SAVED_PATH = "trained_models/3000epochs01"
+OUTPUT = "output.mp4"
+
+def test():
+    # Set seed for reproducibility
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(123)
+    else:
+        torch.manual_seed(123)
+
+    # Load model and environment
+    if torch.cuda.is_available():
+        model = torch.load(f"{SAVED_PATH}/tetris_lr01")
+    else:
+        model = torch.load(f"{SAVED_PATH}/tetris_lr01", map_location=lambda storage, loc: storage)
+    model.eval()
+    env = Tetris(width=WIDTH, height=HEIGHT, block_size=BLOCK_SIZE)
+    env.reset()
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    out = cv2.VideoWriter(OUTPUT, cv2.VideoWriter_fourcc(*"MJPG"), FPS, (int(1.5*WIDTH*BLOCK_SIZE), HEIGHT*BLOCK_SIZE))
+    
+    # Test model
+    while True:
+        next_steps = env.get_next_states()
+        next_actions, next_states = zip(*next_steps.items())
+        next_states = torch.stack(next_states)
+        if torch.cuda.is_available():
+            next_states = next_states.cuda()
+        predictions = model(next_states)[:, 0]
+        index = torch.argmax(predictions).item()
+        action = next_actions[index]
+        _, done = env.step(action, render=True, video=out)
+
+        if done:
+            print(f"Score: {env.score} | Cleared lines: {env.cleared_lines} | Tetrominoes: {env.tetrominoes}")
+            env.reset()
+
+if __name__ == "__main__":
+    test()
--- src/train.py
+++ src/train.py
@@ -2,32 +2,36 @@ import argparse
 import os
 import shutil
 from random import random, randint, sample
-
 import numpy as np
 import torch
 import torch.nn as nn
 from tensorboardX import SummaryWriter
-
 from dqn import DQN
 from tetris import Tetris
 from collections import deque
+import matplotlib.pyplot as plt
 
 WIDTH = 10
 HEIGHT = 20
-BLOCK_SIZE = 30
+BLOCK_SIZE = 30 
 BATCH_SIZE = 512
 LR = 1e-3
 GAMMA = 0.99
 INITIAL_EPSILON = 1
 FINAL_EPSILON = 1e-3
 NUM_DECAY_EPOCHS = 2000
-NUM_EPOCHS = 10000
+NUM_EPOCHS = 3000
 SAVE_INTERVAL = 1000
 REPLAY_MEMORY_SIZE = 30000
 LOG_PATH = "tensorboard"
 SAVED_PATH = "trained_models"
 
 def train():
+
+    #Creating the rewards list so we can plot the results later
+    rewards = []
+
+    #Torch seed for reproducibility and configuring the environment
     if torch.cuda.is_available():
         torch.cuda.manual_seed(123)
     else:
@@ -40,36 +44,38 @@ def train():
     model = DQN()
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     criterion = nn.MSELoss()
-
     state = env.reset()
     if torch.cuda.is_available():
         model.cuda()
         state = state.cuda()
 
+    #Training loop
     replay_memory = deque(maxlen=30000)
     epoch = 0
     while epoch < NUM_EPOCHS:
         next_steps = env.get_next_states()
 
-        # Exploration or exploitation
-        epsilon =  + (max(NUM_EPOCHS - epoch, 0) * (
-                INITIAL_EPSILON - FINAL_EPSILON) / NUM_DECAY_EPOCHS)
+        #Exploration vs exploitation
+        epsilon = FINAL_EPSILON + (max(NUM_EPOCHS/2 - epoch, 0) * (INITIAL_EPSILON - FINAL_EPSILON) / NUM_DECAY_EPOCHS) # Linear decay proportinal to the number of epochs over number of decay epochs
         u = random()
         random_action = u <= epsilon
         next_actions, next_states = zip(*next_steps.items())
         next_states = torch.stack(next_states)
+
         if torch.cuda.is_available():
             next_states = next_states.cuda()
         model.eval()
+
         with torch.no_grad():
             predictions = model(next_states)[:, 0]
         model.train()
+
         if random_action:
             index = randint(0, len(next_steps) - 1)
         else:
             index = torch.argmax(predictions).item()
 
-        next_state = next_states[index, :]
+        next_state = next_states[index, :] 
         action = next_actions[index]
 
         reward, done = env.step(action, render=False)
@@ -77,6 +83,7 @@ def train():
         if torch.cuda.is_available():
             next_state = next_state.cuda()
         replay_memory.append([state, reward, next_state, done])
+
         if done:
             final_score = env.score
             final_tetrominoes = env.tetrominoes
@@ -89,6 +96,7 @@ def train():
             continue
         if len(replay_memory) < REPLAY_MEMORY_SIZE / 10:
             continue
+
         epoch += 1
         batch = sample(replay_memory, min(len(replay_memory), BATCH_SIZE))
         state_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
@@ -107,31 +115,46 @@ def train():
             next_prediction_batch = model(next_state_batch)
         model.train()
 
-        y_batch = torch.cat(
-            tuple(reward if done else reward + GAMMA * prediction for reward, done, prediction in
-                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None]
+        y_batch = torch.cat(tuple(reward if done else reward + GAMMA * prediction for reward, done, prediction in
+                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None] #Reward if done, else reward + gamma * prediction
 
         optimizer.zero_grad()
         loss = criterion(q_values, y_batch)
         loss.backward()
         optimizer.step()
 
-        print("Epoch: {}/{}, Action: {}, Score: {}, Tetrominoes {}, Cleared lines: {}".format(
-            epoch,
-            NUM_EPOCHS,
-            action,
-            final_score,
-            final_tetrominoes,
-            final_cleared_lines))
+        print(f"Epoch: {epoch}/{NUM_EPOCHS}, Action: {action}, Score: {final_score}, Tetrominoes {final_tetrominoes}, Cleared lines: {final_cleared_lines}")
         writer.add_scalar('Train/Score', final_score, epoch - 1)
         writer.add_scalar('Train/Tetrominoes', final_tetrominoes, epoch - 1)
         writer.add_scalar('Train/Cleared lines', final_cleared_lines, epoch - 1)
 
         if epoch > 0 and epoch % SAVE_INTERVAL == 0:
-            torch.save(model, "{}/tetris_{}".format(SAVED_PATH, epoch))
+            torch.save(model, f"{SAVED_PATH}/tetris_{epoch}_{LR}_{GAMMA}")
 
-    torch.save(model, "{}/tetris".format(SAVED_PATH))
+        rewards.append((epoch, final_score, final_tetrominoes, final_cleared_lines))
 
+    torch.save(model, f"{SAVED_PATH}/tetris_{LR}_{GAMMA}_{NUM_EPOCHS}")
+
+    return rewards
 
 if __name__ == "__main__":
-    train()
+    r = train()
+
+    epochs, scores, tetrominoes, cleared_lines = zip(*r)
+
+    plt.plot(epochs, scores)
+    plt.xlabel('Epoch')
+    plt.ylabel('Score')
+    plt.savefig(f"graphs/score_{LR}_{GAMMA}_{NUM_EPOCHS}.png")
+
+    plt.clf()
+    plt.plot(epochs, tetrominoes)
+    plt.xlabel('Epoch')
+    plt.ylabel('Tetrominoes')
+    plt.savefig(f'graphs/tetrominoes_{LR}_{GAMMA}_{NUM_EPOCHS}.png')
+
+    plt.clf()
+    plt.plot(epochs, cleared_lines)
+    plt.xlabel('Epoch')
+    plt.ylabel('Cleared lines')
+    plt.savefig(f'graphs/cleared_lines_{LR}_{GAMMA}_{NUM_EPOCHS}.png')  
