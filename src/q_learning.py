f0f7b230f3da1a7b15fd85eace0c303ba8d6c0bc
George D <georgedutra661@gmail.com>
2023-10-30 10:59:06 -0300

    Documents class

--- src/q_learning.py
+++ src/q_learning.py
@@ -1,3 +1,6 @@
+"""Module for training Tetris AI model through reinforcement learning, using the Q-Learning method.
+"""
+
 import cv2
 import torch
 import numpy as np
@@ -11,7 +14,14 @@ BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
 
 class QL_Tetris:
+    """Reinforcement Learning model to play the game Tetris, trained through Tabular Q-Learning.
+    """
     def __init__(self, stats = 100):
+        """Creates a new model class.
+
+        :param stats: Defines the interval of episodes the model will use to extract data and plot the graphics, defaults to 100
+        :type stats: int, optional
+        """
         # Tetris environment
         self.env = Tetris(width=BOARD_WIDTH, height=BOARD_HEIGHT, block_size=30)
         self.env.reset()
@@ -24,16 +34,31 @@ class QL_Tetris:
         self.stats_interval = stats
 
         # Shape of the Observation Space
-        self.observation_space_dim = (5, (BOARD_WIDTH-1)*(BOARD_HEIGHT-1)+10, (BOARD_WIDTH//2)*BOARD_HEIGHT+10, (BOARD_WIDTH)*(BOARD_HEIGHT)+10) # I added 10 to cover extremely rare 
+        self.observation_space_dim = (5, (BOARD_WIDTH-1)*(BOARD_HEIGHT-1)+10, (BOARD_WIDTH//2)*BOARD_HEIGHT+10, (BOARD_WIDTH)*(BOARD_HEIGHT)+10) # I added 10 to cover extremely rare cases
         self.reset_q_table(self.observation_space_dim)
 
     def reset_q_table(self, shape):
-        self.q_table = np.random.uniform(low = -2, high = 0, size = shape) # Populating the initial q-table with random values
+        """Clear the model's q_table, reseting all the learning from scratch. Certify to save your table's progress before doing so.
 
+        :param shape: Tuple or list with integers, defining the observation space shape.
+        :type shape: tuple[int]
+        """
+        self.q_table = np.random.uniform(low = -2, high = 0, size = shape) # Populating the initial q-table with random values
+    
     def load_q_table(self, filename):
+        """Load a .npy or .npz archive to use as q_table.
+
+        :param filename: Name of the file, with extension. The file must be in the 'graphs/q_learn' folder.
+        :type filename: str
+        """
         self.q_table = np.load(f"./qtables/{filename}")
 
     def plot_rewards(self, savefile = None):
+        """Creates a plot using the data stored in self.aggr_ep_rewards.
+
+        :param savefile: Name of the file, without extension. If passed, saves an image with the passed name. Otherwise, opens a cv2 image, defaults to None
+        :type savefile: str, optional
+        """
         plt.plot(self.aggr_ep_rewards['ep'], self.aggr_ep_rewards['avg'], label="average rewards")
         plt.plot(self.aggr_ep_rewards['ep'], self.aggr_ep_rewards['max'], label="max rewards")
         plt.plot(self.aggr_ep_rewards['ep'], self.aggr_ep_rewards['min'], label="min rewards")
@@ -46,10 +71,31 @@ class QL_Tetris:
         plt.clf()
 
     def clean_rewards(self):
+        """Resets plotting data.
+        """
         self.total_rewards = []
         self.aggr_ep_rewards = {'ep': [], 'avg': [], 'max': [], 'min': []}
 
     def train(self, learn_rate, discount, max_episodes, target = np.inf, save_q_table = 10000, plot = True, verbose = 1,  render_interval = 1000):
+        """Main training method, using .
+
+        :param learn_rate: alpha parameter inserted into the new_q formula.
+        :type learn_rate: float
+        :param discount: gamma parameter inserted into the new_q formula.
+        :type discount: float
+        :param max_episodes: Number of epochs to use in the training.
+        :type max_episodes: int
+        :param target: Defines a maximum score to the model, to avoid infinite episodes, defaults to np.inf
+        :type target: int, optional
+        :param save_q_table: Episodes interval after wich the model will save the q_table's progress into a .npy file in the 'q_table' folder, defaults to 10000
+        :type save_q_table: int, optional
+        :param plot: Activates or deactivates the graph's plotting, defaults to True
+        :type plot: bool, optional
+        :param verbose: Defines how much info the model will print into the terminal. 0 never prints info. 1 prints info each 1000 episodes. 2 or higher prints info each time stats are collected, defaults to 1
+        :type verbose: int, optional
+        :param render_interval: Episodes interval after wich the model will activate rendering, recording a gameplay video, defaults to 1000
+        :type render_interval: int, optional
+        """
         self.clean_rewards()
         self.env.reset()
 
@@ -119,9 +165,8 @@ class QL_Tetris:
                     self.q_table[current_q_loc[0]][current_q_loc[1]][current_q_loc[2]][current_q_loc[3]] = new_q # Insert the updated value into the table
             
             # update the epsilon parameter
-            if end_epsilon_decay >= episode >= start_epsilon_decay:
+            if end_epsilon_decay >= episode >= start_epsilon_decay and epsilon > 0.1:
                 epsilon -= epsilon_decay_value
-            else: epsilon = 0.10
 
             self.total_rewards.append(episode_reward)
             if verbose > 1: print(f"Episode {episode}/{max_episodes} reward: {episode_reward}")
@@ -144,6 +189,11 @@ class QL_Tetris:
         if plot: self.plot_rewards()
 
     def parameter_analysis(self, epochs):
+        """Tests alpha and gamma parameters from 0.1 to 0.9, and creates plots to show results. Takes a lot of time depending of the hardware.
+
+        :param epochs: How many epochs episodes will be used to each training
+        :type epochs: int
+        """
         original_output = self.out
         self.out = None # Deactivate video
 
@@ -161,6 +211,8 @@ class QL_Tetris:
         self.out = original_output # Reactivate video
 
     def play(self):
+        """Plays one game using the current q_table as policy. Records a video called test.mp4 with your gameplay.
+        """
         output = cv2.VideoWriter("test.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 60,
                                    (int(1.5*10*30), 20*30))
         self.env.reset()
@@ -204,13 +256,12 @@ class QL_Tetris:
 if __name__ == "__main__":
     q_model = QL_Tetris()
 
-    q_model.load_q_table("first_1M.npy")
-    q_model.play()
+    # q_model.load_q_table("first_1M.npy")
+    # q_model.play()
 
-    # q_model.train(learn_rate=0.4,
-    #             discount=0.95,
-    #             max_episodes=1000000,
-    #             target=10000,
-    #             save_q_table=50000)
+    q_model.train(learn_rate=0.4,
+                discount=0.95,
+                max_episodes=20000,
+                target=10000)
     
     # q_model.parameter_analysis(10000)
